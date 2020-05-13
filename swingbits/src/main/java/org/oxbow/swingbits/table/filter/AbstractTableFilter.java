0dd52f9260fb77657b05602567c4c83618ec5fa6
Joerg, Andreas <andreas.joerg@nuernberger.de>
2020-05-13 13:00:19 +0200

    Format

--- swingbits/src/main/java/org/oxbow/swingbits/table/filter/AbstractTableFilter.java
+++ swingbits/src/main/java/org/oxbow/swingbits/table/filter/AbstractTableFilter.java
@@ -31,20 +31,28 @@
 
 package org.oxbow.swingbits.table.filter;
 
-import javax.swing.*;
+import static org.oxbow.swingbits.util.CollectionUtils.*;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.swing.JTable;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 import javax.swing.table.TableModel;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.util.*;
-
-import static org.oxbow.swingbits.util.CollectionUtils.isEmpty;
 
 /**
  * Partial implementation of table filter
  *
  * Created on Feb 10, 2011
+ * 
  * @author Eugene Ryzhikov
  *
  * @param <T>
@@ -52,131 +60,135 @@ import static org.oxbow.swingbits.util.CollectionUtils.isEmpty;
 @SuppressWarnings("serial")
 public abstract class AbstractTableFilter<T extends JTable> implements ITableFilter<T> {
 
-    private final Set<IFilterChangeListener> listeners = Collections.synchronizedSet( new HashSet<IFilterChangeListener>());
+    private final Set<IFilterChangeListener> listeners = Collections
+	    .synchronizedSet(new HashSet<IFilterChangeListener>());
 
-    private final Map<Integer, Collection<DistinctColumnItem>> distinctItemCache =
-            Collections.synchronizedMap(new HashMap<Integer, Collection<DistinctColumnItem>>());
+    private final Map<Integer, Collection<DistinctColumnItem>> distinctItemCache = Collections
+	    .synchronizedMap(new HashMap<Integer, Collection<DistinctColumnItem>>());
 
     private final T table;
     private final TableFilterState filterState = new TableFilterState();
 
-    public AbstractTableFilter( T table ) {
-        this.table = table;
-        setupDistinctItemCacheRefresh();
+    public AbstractTableFilter(T table) {
+	this.table = table;
+	setupDistinctItemCacheRefresh();
     }
 
-
     private void setupDistinctItemCacheRefresh() {
-        clearDistinctItemCache();
-        listenForDataChange( table.getModel() );
-        listenForModelChange();
+	clearDistinctItemCache();
+	listenForDataChange(table.getModel());
+	listenForModelChange();
     }
 
     private void listenForModelChange() {
-        table.addPropertyChangeListener("model", new PropertyChangeListener() {
-            @Override
-            public void propertyChange(PropertyChangeEvent e) {
-                clearDistinctItemCache();
-                listenForDataChange( (TableModel) e.getNewValue() );
-            }
-        });
+	table.addPropertyChangeListener("model", new PropertyChangeListener() {
+	    @Override
+	    public void propertyChange(PropertyChangeEvent e) {
+		clearDistinctItemCache();
+		listenForDataChange((TableModel) e.getNewValue());
+	    }
+	});
     }
 
     private void listenForDataChange(TableModel model) {
-        if (model != null) {
-            model.addTableModelListener(new TableModelListener() {
-                @Override
-                public void tableChanged(TableModelEvent e) {
-                    clearDistinctItemCache();
-                }
-            });
-        }
+	if (model != null) {
+	    model.addTableModelListener(new TableModelListener() {
+		@Override
+		public void tableChanged(TableModelEvent e) {
+		    clearDistinctItemCache();
+		}
+	    });
+	}
     }
 
-
     private void clearDistinctItemCache() {
-        distinctItemCache.clear();
+	distinctItemCache.clear();
     }
 
     @Override
     public T getTable() {
-        return table;
+	return table;
     }
 
     protected abstract boolean execute(int col, Collection<DistinctColumnItem> items);
 
     @Override
     public boolean apply(int col, Collection<DistinctColumnItem> items) {
-        setFilterState(col, items);
-        boolean result = false;
-        if ( result = execute( col, items ) ) fireFilterChange();
-        return result;
-    }
-
-
-    @Override
-    public final void addChangeListener( IFilterChangeListener listener ) {
-        if ( listener != null ) listeners.add(listener);
+	setFilterState(col, items);
+	boolean result = false;
+	if (result = execute(col, items)) {
+	    fireFilterChange();
+	}
+	return result;
     }
 
     @Override
-    public final void removeChangeListener( IFilterChangeListener listener ) {
-        if ( listener != null ) listeners.remove(listener);
+    public final void addChangeListener(IFilterChangeListener listener) {
+	if (listener != null) {
+	    listeners.add(listener);
+	}
+    }
+
+    @Override
+    public final void removeChangeListener(IFilterChangeListener listener) {
+	if (listener != null) {
+	    listeners.remove(listener);
+	}
     }
 
     public final void fireFilterChange() {
-        for( IFilterChangeListener l: listeners ) {
-            l.filterChanged(AbstractTableFilter.this);
-        }
+	for (IFilterChangeListener l : listeners) {
+	    l.filterChanged(AbstractTableFilter.this);
+	}
     }
 
     @Override
-    public Collection<DistinctColumnItem> getDistinctColumnItems( int column ) {
-        Collection<DistinctColumnItem> result = distinctItemCache.get(column);
-        if ( result == null ) {
-            result = collectDistinctColumnItems( column );
-            distinctItemCache.put(column, result);
-        }
-        return result;
+    public Collection<DistinctColumnItem> getDistinctColumnItems(int column) {
+	Collection<DistinctColumnItem> result = distinctItemCache.get(column);
+	if (result == null) {
+	    result = collectDistinctColumnItems(column);
+	    distinctItemCache.put(column, result);
+	}
+	return result;
 
     }
 
-    private Collection<DistinctColumnItem> collectDistinctColumnItems( int column ) {
-        Set<DistinctColumnItem> result = new TreeSet<DistinctColumnItem>(); // to collect unique items
-        for( int row=0; row<table.getModel().getRowCount(); row++) {
-            Object value = table.getModel().getValueAt( row, column);
-            result.add(new DistinctColumnItem(value, row));
-        }
-        return result;
-    }
-
-    @Override
-    public Collection<DistinctColumnItem> getFilterState( int column ) {
-        return filterState.getValues(column);
+    private Collection<DistinctColumnItem> collectDistinctColumnItems(int column) {
+	Set<DistinctColumnItem> result = new TreeSet<DistinctColumnItem>(); // to collect unique items
+	for (int row = 0; row < table.getModel().getRowCount(); row++) {
+	    Object value = table.getModel().getValueAt(row, column);
+	    result.add(new DistinctColumnItem(value, row));
+	}
+	return result;
     }
 
     @Override
-    public boolean isFiltered( int column ) {
-        Collection<DistinctColumnItem> checks = getFilterState( column );
-        return !isEmpty(checks) && getDistinctColumnItems( column ).size() != checks.size();
+    public Collection<DistinctColumnItem> getFilterState(int column) {
+	return filterState.getValues(column);
     }
 
     @Override
-    public boolean includeRow( ITableFilter.Row row ) {
-        return filterState.include(row);
+    public boolean isFiltered(int column) {
+	Collection<DistinctColumnItem> checks = getFilterState(column);
+	return !isEmpty(checks) && getDistinctColumnItems(column).size() != checks.size();
     }
 
-    public void setFilterState(int column, Collection<DistinctColumnItem> values ) {
-        filterState.setValues(column, values);
+    @Override
+    public boolean includeRow(ITableFilter.Row row) {
+	return filterState.include(row);
+    }
+
+    public void setFilterState(int column, Collection<DistinctColumnItem> values) {
+	filterState.setValues(column, values);
     }
 
     public void clear() {
-        filterState.clear();
-        Collection<DistinctColumnItem> items = Collections.emptyList();
-        for( int column=0; column<table.getModel().getColumnCount(); column++) {
-            execute(column, items);
-        }
-        fireFilterChange();
+	filterState.clear();
+	Collection<DistinctColumnItem> items = Collections.emptyList();
+	for (int column = 0; column < table.getModel().getColumnCount(); column++) {
+	    execute(column, items);
+	}
+	fireFilterChange();
     }
 
 }
