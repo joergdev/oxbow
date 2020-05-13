0c6a8e460a435e7564c7fee2bf7e417db1152bb7
Joerg, Andreas <andreas.joerg@nuernberger.de>
2020-05-13 11:43:28 +0200

    format

--- swingbits/src/main/java/org/oxbow/swingbits/table/filter/TableRowFilterSupport.java
+++ swingbits/src/main/java/org/oxbow/swingbits/table/filter/TableRowFilterSupport.java
@@ -29,7 +29,6 @@
  *
  */
 
-
 package org.oxbow.swingbits.table.filter;
 
 import java.beans.PropertyChangeEvent;
@@ -57,145 +56,148 @@ public final class TableRowFilterSupport {
     private int filterIconPlacement = SwingConstants.LEADING;
     private boolean useTableRenderers = false;
 
-    private TableRowFilterSupport( ITableFilter<?> filter ) {
-        if ( filter == null ) throw new NullPointerException();
-        //this.table = table;
-        this.filter = filter;
+    private TableRowFilterSupport(ITableFilter<?> filter) {
+	if (filter == null) {
+	    throw new NullPointerException();
+	}
+	// this.table = table;
+	this.filter = filter;
     }
 
-    public static TableRowFilterSupport forTable( JTable table ) {
-        return new TableRowFilterSupport(new JTableFilter(table));
+    public static TableRowFilterSupport forTable(JTable table) {
+	return new TableRowFilterSupport(new JTableFilter(table));
     }
 
-    public static TableRowFilterSupport forFilter( ITableFilter<?> filter ) {
-        return new TableRowFilterSupport(filter);
+    public static TableRowFilterSupport forFilter(ITableFilter<?> filter) {
+	return new TableRowFilterSupport(filter);
     }
 
     /**
      * Additional actions visible in column filter list
+     * 
      * @param visible
      * @return
      */
-    public TableRowFilterSupport actions( boolean visible ) {
-        this.actionsVisible = visible;
-        return this;
+    public TableRowFilterSupport actions(boolean visible) {
+	this.actionsVisible = visible;
+	return this;
     }
 
     /**
-     * Set the placement of the filter icon with respect to the existing icon
-     * in the column label.
+     * Set the placement of the filter icon with respect to the existing icon in the
+     * column label.
      *
      * @param filterIconPlacement either SwingConstants.LEADING or
-     *         SwingConstants.TRAILING.
+     *                            SwingConstants.TRAILING.
      * @return this
      */
     public TableRowFilterSupport filterIconPlacement(int filterIconPlacement) {
-        if (filterIconPlacement != SwingConstants.LEADING &&
-                filterIconPlacement != SwingConstants.TRAILING) {
-            throw new UnsupportedOperationException("The filter icon " +         
-                    "placement can only take the values of " +                   
-                    "SwingConstants.LEADING or SwingConstants.TRAILING");
-        }
-        this.filterIconPlacement = filterIconPlacement;
-        return this;
+	if (filterIconPlacement != SwingConstants.LEADING && filterIconPlacement != SwingConstants.TRAILING) {
+	    throw new UnsupportedOperationException("The filter icon " + "placement can only take the values of "
+		    + "SwingConstants.LEADING or SwingConstants.TRAILING");
+	}
+	this.filterIconPlacement = filterIconPlacement;
+	return this;
     }
 
     /**
      * Column filter list is searchable
+     * 
      * @param searchable
      * @return
      */
-    public TableRowFilterSupport searchable( boolean searchable ) {
-        this.searchable = searchable;
-        return this;
+    public TableRowFilterSupport searchable(boolean searchable) {
+	this.searchable = searchable;
+	return this;
     }
 
     public TableRowFilterSupport searchFilter(IListFilter searchFilter) {
-        this.searchFilter = searchFilter;
-        return this;
+	this.searchFilter = searchFilter;
+	return this;
     }
 
-    public TableRowFilterSupport searchTransalator( IObjectToStringTranslator translator ) {
-        this.translator = translator;
-        return this;
+    public TableRowFilterSupport searchTransalator(IObjectToStringTranslator translator) {
+	this.translator = translator;
+	return this;
     }
 
-    public TableRowFilterSupport useTableRenderers( boolean value ) {
-        this.useTableRenderers = value;
-        return this;
+    public TableRowFilterSupport useTableRenderers(boolean value) {
+	this.useTableRenderers = value;
+	return this;
     }
 
     public TableRowFilterSupport onFilterChange(IFilterChangeListener listener) {
-        this.filter.addChangeListener( listener );
-        return this;
+	this.filter.addChangeListener(listener);
+	return this;
     }
 
     public JTable apply() {
 
-        final TableFilterColumnPopup filterPopup = new TableFilterColumnPopup(filter);
-        filterPopup.setEnabled(true);
-        filterPopup.setActionsVisible(actionsVisible);
-        filterPopup.setSearchable(searchable);
-        filterPopup.setSearchFilter(searchFilter);
-        filterPopup.setSearchTranslator(translator);
-        filterPopup.setUseTableRenderers( useTableRenderers );
+	final TableFilterColumnPopup filterPopup = new TableFilterColumnPopup(filter);
+	filterPopup.setEnabled(true);
+	filterPopup.setActionsVisible(actionsVisible);
+	filterPopup.setSearchable(searchable);
+	filterPopup.setSearchFilter(searchFilter);
+	filterPopup.setSearchTranslator(translator);
+	filterPopup.setUseTableRenderers(useTableRenderers);
 
-        setupTableHeader();
+	setupTableHeader();
 
-        return filter.getTable();
+	return filter.getTable();
     }
 
     private void setupTableHeader() {
 
-        final JTable table = filter.getTable();
+	final JTable table = filter.getTable();
 
-        filter.addChangeListener(new IFilterChangeListener() {
+	filter.addChangeListener(new IFilterChangeListener() {
 
-            @Override
-            public void filterChanged(ITableFilter<?> filter) {
-                table.getTableHeader().repaint();
+	    @Override
+	    public void filterChanged(ITableFilter<?> filter) {
+		table.getTableHeader().repaint();
 
-            }
-        });
+	    }
+	});
 
-        // make sure that search component is reset after table model changes
-        setupHeaderRenderers(table.getModel(), true );
+	// make sure that search component is reset after table model changes
+	setupHeaderRenderers(table.getModel(), true);
     }
 
     public void applyColumnFilters(Map<Integer, Set<DistinctColumnItem>> columnFilters) {
 
-        if ( columnFilters == null ) return;
+	if (columnFilters == null) {
+	    return;
+	}
 
-        for ( Integer key : columnFilters.keySet()) {
-            Set<DistinctColumnItem> checked = columnFilters.get(key);
-            this.filter.apply(key, checked);
-        }
+	for (Integer key : columnFilters.keySet()) {
+	    Set<DistinctColumnItem> checked = columnFilters.get(key);
+	    this.filter.apply(key, checked);
+	}
     }
 
+    private void setupHeaderRenderers(TableModel newModel, boolean fullSetup) {
 
-    private void setupHeaderRenderers( TableModel newModel, boolean fullSetup ) {
+	JTable table = filter.getTable();
 
-        JTable table =  filter.getTable();
+	FilterTableHeaderRenderer headerRenderer = new FilterTableHeaderRenderer(filter, filterIconPlacement);
+	filter.modelChanged(newModel);
 
-        FilterTableHeaderRenderer headerRenderer =
-                new FilterTableHeaderRenderer(filter, filterIconPlacement);
-        filter.modelChanged( newModel );
+	for (TableColumn c : Collections.list(table.getColumnModel().getColumns())) {
+	    c.setHeaderRenderer(headerRenderer);
+	}
 
-        for( TableColumn c:  Collections.list( table.getColumnModel().getColumns()) ) {
-            c.setHeaderRenderer( headerRenderer );
-        }
+	if (!fullSetup) {
+	    return;
+	}
 
-        if ( !fullSetup ) return;
+	table.addPropertyChangeListener("model", new PropertyChangeListener() {
 
-        table.addPropertyChangeListener("model", new PropertyChangeListener() {
+	    public void propertyChange(PropertyChangeEvent e) {
+		setupHeaderRenderers((TableModel) e.getNewValue(), false);
+	    }
 
-            public void propertyChange(PropertyChangeEvent e) {
-                setupHeaderRenderers( (TableModel) e.getNewValue(), false );
-            }
-
-        });
+	});
 
     }
-
 
 }
